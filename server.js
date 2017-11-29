e92795b3c53aaed066392255773a83851072599c
jjoyson <jjoyson@hawk.iit.edu>
2017-11-29 02:13:49 -0600

    Got Post to work

--- server.js
+++ server.js
@@ -1,35 +1,70 @@
+var express = require('express');
+var app     = express();
 var http = require('http');
-var express = require("express");
-var pg = require('pg');
-var app = express();
-var constring = 'postgres://postgres:4jw3np4h@localhost:8888/mydb'
-var pg = require('pg');
-var conString = "postgres://YourUserName:YourPassword@localhost:5432/YourDatabase";
+var flag = -1;
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var pg = require('pg');
+var conString = "postgres://postgres:postgres@localhost:5432/AirCobra";
 var client = new pg.Client(conString);
+/*
+client.connect();
 
-
-http.createServer(function (request, response) {
-   response.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin' : '*'
-    });
-    response.end('Hello World\n');
-}).listen(8080);
+const text = 'INSERT INTO customer (firstname,lastname,email,address,creditcardnumber,hiata) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+const values = ['John','Smith','example2@mail.com','Fake Address',1234568,'ORD'];
+client.query(text, values, (err, res) => {
+  if (err) {
+    console.log (err.stack)
+    flag = 0
+    client.end()
+  } else{
+    console.log(res.rows[0])
+    flag = 1
+    client.end()
+  }
+});*/
 
 app.post('/myaction', function(req, res) {
     client.connect();
-    console.log("IM HERE");
-    client.query("INSERT INTO customer(firstname,lastname,email,address,creditcardnumber,hiata) value($1,$2,$3,$4,$5,$6)"
-    ,[req.fname, req.lname, req.email, req.add, /*parse into integer*/req.credit, req.hiata]);
+    var credit = parseInt(req.body.credit);
+    const text = 'INSERT INTO customer (firstname,lastname,email,address,creditcardnumber,hiata) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+    const values = [req.body.fname,req.body.lname,req.body.email,req.body.add,credit,req.body.hiata];
+    client.query(text, values, (err, res) => {
+      if (err) {
+        console.log (err.stack)
+        flag = 0
+      } else{
+        console.log(res.rows[0])
+        flag = 1
+      }
+    });
+    const text2 = 'INSERT INTO creditcard (email,creditcardnumber,paymentaddress) VALUES($1, $2, $3) RETURNING *';
+    const values2 = [req.body.email,credit,req.body.add];
+    client.query(text2, values2, (err, res) => {
+      if (err) {
+        console.log (err.stack)
+        flag = 0
+      } else{
+        console.log(res.rows[0])
+        flag = 1
+      }
+    });
+    if(flag > -1){
+      clientExit();
+      res.redirect('public/login');
+    }
 });
-/*
+
+function clientExit(){
+  client.end();
+}
+
 app.get('/youraction', function(req, res){
     client.connect();
-    client.query("SELECT FROM ")
-})*/
-    
-
+    const query = client.query("SELECT FROM customer WHERE customer.email = '"+req.body.email+"' AND customer.firstname = '"+req.body.fname+"' AND customer.lastname = '"+req.body.lname+"'");
+})
 
 /*var constring = 'postgres://postgres:4jw3np4h@localhost:8888/mydb';
 pg.connect(conString, function(err, client) {
@@ -38,3 +73,7 @@ pg.connect(conString, function(err, client) {
     client.query("INSERT INTO table(a,b,c,d) value('a','b','c','d')");
 
 var query = client.query("SELECT * FROM table");*/
+    app.listen(8080, function() {
+    console.log('Server running at http://127.0.0.1:8080/');
+  });
+
