82284b36a6dba3058cc630097e3c8acdb931baa8
jjoyson <jjoyson@hawk.iit.edu>
2017-11-29 14:42:29 -0600

    Post changed

--- server.js
+++ server.js
@@ -8,66 +8,43 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var pg = require('pg');
-var conString = "postgres://postgres:4jw3np4h@localhost:5432/425Project";
+var conString = "postgres://postgres:postgres@localhost:5432/AirCobra";
+var client = new pg.Client(conString);
 
-/*
-client.connect();
-
-const text = 'INSERT INTO customer (firstname,lastname,email,address,creditcardnumber,hiata) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
-const values = ['John','Smith','example2@mail.com','Fake Address',1234568,'ORD'];
-client.query(text, values, (err, res) => {
-  if (err) {
-    console.log (err.stack)
-    flag = 0
-    client.end()
-  } else{
-    console.log(res.rows[0])
-    flag = 1
-    client.end()
-  }
-});*/
-
-app.post('/myaction', function(req, res) {
-    client.connect();
-    var credit = parseInt(req.body.credit);
-    const text = 'INSERT INTO customer (firstname,lastname,email,address,creditcardnumber,hiata) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
-    const values = [req.body.fname,req.body.lname,req.body.email,req.body.add,credit,req.body.hiata];
-    client.query(text, values, (err, res) => {
-      if (err) {
-        console.log (err.stack)
-        flag = 0
-      } else{
-        console.log(res.rows[0])
-        flag = 1
-      }
-    });
-    const text2 = 'INSERT INTO creditcard (email,creditcardnumber,paymentaddress) VALUES($1, $2, $3) RETURNING *';
-    const values2 = [req.body.email,credit,req.body.add];
-    client.query(text2, values2, (err, res) => {
-      if (err) {
-        console.log (err.stack)
-        flag = 0
-      } else{
-        console.log(res.rows[0])
-        flag = 1
-      }
-    });
-    if(flag > -1){
-      clientExit()
-      //res.redirect('public/update_information')
-      res.end()
+app.post('/myaction', function(req, response) {
+  client.connect();
+  var credit = parseInt(req.body.credit);
+  const text = 'INSERT INTO customer (firstname,lastname,email,address,creditcardnumber,hiata) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+  const values = [req.body.fname,req.body.lname,req.body.email,req.body.add,credit,req.body.hiata];
+  email = req.body.email;
+  client.query(text, values, (err, res) => {
+    if (err) {
+      console.log (err.stack)
+    } else{
+      console.log(res.rows[0])
     }
+  });
+  const text2 = 'INSERT INTO creditcard (email,creditcardnumber,paymentaddress) VALUES($1, $2, $3) RETURNING *';
+  const values2 = [req.body.email,credit,req.body.add];
+  client.query(text2, values2, (err, res) => {
+    if (err) {
+      console.log (err.stack)
+    } else{
+      console.log(res.rows[0]);
+      client.end();
+      response.sendFile('Web_design/update_information.html', {root: __dirname });
+    }
+  });
 });
 
 
 app.get('/youraction', function(req, res){
-  runSQL("SELECT email,firstname FROM customer WHERE customer.email = '"+req.body.email+"' AND customer.firstname = '"+req.body.fname+"'").then(function(err, res) {
-    if(err){
-      console.log("there was an error");
-    }
-    else 
-      console.log(res);
-  }); 
+  client.connect();
+  var query = client.query("SELECT email,firstname FROM customer WHERE customer.email = 'saikiran@hawk.iit.edu' AND customer.firstname = 'Sai'", function(err, res) {
+      if(err) console.log(err);
+      else console.log("Logged In");
+  });
+  client.end();
 });
 
 /*var constring = 'postgres://postgres:4jw3np4h@localhost:8888/mydb';
@@ -80,36 +57,3 @@ var query = client.query("SELECT * FROM table");*/
     app.listen(8080, function() {
     console.log('Server running at http://127.0.0.1:8080/');
   });
-
-  
-function runSQL (sqlStatement) {
-  var deferred = q.defer(); 
-  var results = [];
-
-  // Get a Postgres client from the connection pool
-  pg.connect(conString, function(err, client, done) {
-
-
-      // SQL Query > Select Data
-      var query = client.query(sqlStatement, function(err, res) {
-          if(err) console.log(err);
-          deferred.resolve(res);
-      });
-
-
-      // After all data is returned, close connection and return results
-      query.on('end', function() {
-          client.end();
-          deferred.resolve(results);
-      });
-
-      // Handle Errors
-      if(err) {
-        console.log(err);
-      }
-
-  });
-
-  return deferred.promise;
-};
-
