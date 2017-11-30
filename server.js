6c153422fe5ca263f2b5c50c97ae5663870f74df
jjoyson <jjoyson@hawk.iit.edu>
2017-11-29 19:20:30 -0600

    Fully Works
    Make sure to change:
      Database name,
      Database password

--- server.js
+++ server.js
@@ -1,68 +1,129 @@
-var express = require('express');
-var app     = express();
-var http = require('http');
-var flag = -1;
-var q = require('q');
+var express = require('express')
+var app     = express()
+var http = require('http')
+var flag = -1
+var q = require('q')
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
-var pg = require('pg');
-var conString = "postgres://postgres:postgres@localhost:5432/AirCobra";
-var client = new pg.Client(conString);
+var pg = require('pg')
+var conString = "postgres://postgres:postgres@localhost:5432/AirCobra"
+var client = new pg.Client(conString)
+var email = ''
+
+app.post('/login', function(req, response) {
+  pg.connect(conString, function (err, client, done) {
+    if (err) {
+      return console.error('could not connect to postgres', err)
+    } 
+    var credit = parseInt(req.body.credit);
+    const tex = 'SELECT FROM customer WHERE email = $1 AND firstname = $2'
+    const value = [req.body.email,req.body.fname]
+    client.query(tex, value, (err, res) => {
+      if (err) {
+        console.log (err.stack)
+        done
+      } else{
+        console.log(res.rows[0])
+        var check = res.rows[0]
+        done
+        if (typeof(check) != "undefined"){
+          email = req.body.email
+          response.sendFile('Web_design/update_information.html', {root: __dirname })
+        }
+        else{
+          response.sendFile('Web_design/wlogin.html', {root: __dirname })
+        }
+      }
+    })
+  })
+})
 
 app.post('/myaction', function(req, response) {
-  client.connect();
-  var credit = parseInt(req.body.credit);
-  const text = 'INSERT INTO customer (firstname,lastname,email,address,creditcardnumber,hiata) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
-  const values = [req.body.fname,req.body.lname,req.body.email,req.body.add,credit,req.body.hiata];
-  email = req.body.email;
-  client.query(text, values, (err, res) => {
+  pg.connect(conString, function (err, client, done) {
     if (err) {
-      console.log (err.stack)
-    } else{
-      console.log(res.rows[0])
-    }
-  });
-  const text2 = 'INSERT INTO creditcard (email,creditcardnumber,paymentaddress) VALUES($1, $2, $3) RETURNING *';
-  const values2 = [req.body.email,credit,req.body.add];
-  client.query(text2, values2, (err, res) => {
-    if (err) {
-      console.log (err.stack)
-    } else{
-      console.log(res.rows[0]);
-      client.end();
-      response.sendFile('Web_design/update_information.html', {root: __dirname });
-    }
-  });
-});
+      return console.error('could not connect to postgres', err)
+    } 
+    var credit = parseInt(req.body.credit);
+    const tex = 'INSERT INTO customer (firstname,lastname,email,address,hiata) VALUES($1, $2, $3, $4, $5) RETURNING *'
+    const value = [req.body.fname,req.body.lname,req.body.email,req.body.add,req.body.hiata]
+    email = req.body.email
+    client.query(tex, value, (err, res) => {
+      if (err) {
+        console.log (err.stack)
+        done
+      } else{
+        console.log(res.rows[0])
+        done
+      }
+    })
+    response.sendFile('Web_design/update_information.html', {root: __dirname })
+  })
+})
 
-app.get('/youraction', function(req, res){
-  console.log("IM HERE");
-  client.connect();
-  text = "SELECT email,firstname FROM customer WHERE customer.email = $1 AND customer.firstname = $2";
-  values = ['test1@test.com','dingu'];
-  client.query(text, values, (err, res) => {
+app.post('/updateInfo', function(req, response) {
+  pg.connect(conString, function (err, client, done) {
     if (err) {
-      console.log (err.stack);
-    } else{
-      console.log(res.rows[1]);
-    }
-  });
-  /*var query = client.query("SELECT email,firstname FROM customer WHERE customer.email = 'test1@test.com' AND customer.firstname = 'dingus'")
-  query.on("row", function(row, result) {
-      result.addRow(row);
-  });
-  console.log("IM HERE");
-  query.on("end", function(result) {
-    var a = JSON.stringify(result.rows, null, "   ");
-    if(a[1] == ']')
-      console.log("Not there");
-    else 
-      console.log(a);
-    client.end();
-  });*/
+      return console.error('could not connect to postgres', err)
+    } 
+    const text = "UPDATE customer SET firstname = $1, lastname = $2, address = $3, email = $4, hiata = $5 WHERE email = $6 RETURNING *"
+    const values = [req.body.fname,req.body.lname,req.body.add,req.body.email,req.body.hiata,email]
+    client.query(text, values, (err, res) => {
+      if (err) {
+        console.log (err.stack)
+        done
+      } else{
+        console.log(res.rows[0])
+        email = req.body.email
+        done
+      }
+    })
+    response.sendFile('Web_design/update_information.html', {root: __dirname })
+  })
+})
+  
+app.post('/deleteCredit', function(req, response) {
+  pg.connect(conString, function (err, client, done) {
+    if (err) {
+      return console.error('could not connect to postgres', err)
+    } 
+    var credit = parseInt(req.body.credit)
+    const text5 = 'DELETE FROM creditcard WHERE email = $3 AND (creditcardnumber = $1 OR paymentaddress = $2) RETURNING *'
+    const values5 = [credit,req.body.dbadd,email]
+    client.query(text5, values5, (err, res) => {
+      if (err) {
+        console.log (err.stack)
+        done
+      } else{
+        console.log(res.rows[0])
+        done
+      }
+    })
+    response.sendFile('Web_design/update_information.html', {root: __dirname })
+  })
+})
+
+app.post('/addCredit', function(req, response) {
+  pg.connect(conString, function (err, client, done) {
+    if (err) {
+      return console.error('could not connect to postgres', err)
+    } 
+    var credit = parseInt(req.body.credit)
+    const text4 = 'INSERT INTO creditcard (email,creditcardnumber,paymentaddress) VALUES($1, $2, $3) RETURNING *'
+    const values4 = [email,credit,req.body.badd]
+    client.query(text4, values4, (err, res) => {
+      if (err) {
+        console.log (err.stack)
+        done
+      } else{
+        console.log(res.rows[0])
+        done
+      }
+    })
+    response.sendFile('Web_design/update_information.html', {root: __dirname })
+  })
+})
+  app.listen(8080, function() {
+  console.log('Server running at http://127.0.0.1:8080/');
 });
-    app.listen(8080, function() {
-    console.log('Server running at http://127.0.0.1:8080/');
-  });
